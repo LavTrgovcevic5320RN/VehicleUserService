41654484e36720b3b55140dbc82b4f76bcd2b5ea
Lav Trgovčević RN 53/20 <ltrgovcevic5320rn@raf.rs>
2022-12-20 15:52:30 +0100

    Zapocet projekat

---
+++ src/test/java/com/example/VehicleUserService/VehicleUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VehicleUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
