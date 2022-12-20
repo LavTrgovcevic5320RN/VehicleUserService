d76fa58784164ba568812a9837ea9def2473b9fe
Lav Trgovčević RN 53/20 <ltrgovcevic5320rn@raf.rs>
2022-12-20 20:07:32 +0100

    Napravljeni dto

---
+++ src/main/java/dto/ManagerDto.java
@@ -0,0 +1,97 @@
+package dto;
+
+public class ManagerDto {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    // private Date birthday;
+    private String contact;
+    private String hotelName;
+    private String city;
+    private String verificationCode;
+    private String sendTo;
+    // datum zaposljavanja
+
+    public String getSendTo() {
+        return sendTo;
+    }
+
+    public void setSendTo(String sendTo) {
+        this.sendTo = sendTo;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getHotelName(String hotelName) {
+        return this.hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
